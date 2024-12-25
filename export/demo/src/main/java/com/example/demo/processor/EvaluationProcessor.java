c1d50a482279bdd87cd020ca15b0d1f527272226
info1 <info.mailaddress1@gmail.com>
2024-12-25 17:54:48 +0900

    制御処理の修正

--- export/demo/src/main/java/com/example/demo/processor/EvaluationProcessor.java
+++ export/demo/src/main/java/com/example/demo/processor/EvaluationProcessor.java
@@ -18,17 +18,18 @@ public class EvaluationProcessor implements ItemProcessor<Yorishiro, Yorishiro> 
       return null;
     }
 
-    if (item.getEvaluation() == 0) {
+    if (item.getAge() < 13 && item.getEvaluation() == 0) {
       System.out.println("憑依魔法：" + item.getName() + "にサンタが憑依しました。");
       System.out.println("生成魔法：サンタが" + item.getName() + "のためにプレゼントを生成しました。");
       System.out.println("憑依解除：サンタは" + item.getName() + "から出ていきました。");
-    } else if (item.getEvaluation() == 1) {
-      System.out.println("憑依魔法：" + item.getName() + "には何も憑依しませんでした。");
-    } else {
+    } else if (item.getEvaluation() == 2) {
       System.out.println("憑依魔法：" + item.getName() + "にサタンが憑依しました。");
       System.out.println("呪術　　：サタンが" + item.getName() + "の寿命を吸い取りました。");
       System.out.println("憑依解除：サタンは" + item.getName() + "から出ていきました。");
+    } else {
+      System.out.println("憑依魔法：" + item.getName() + "には何も憑依しませんでした。");
     }
+
     return item;
   }
 }
